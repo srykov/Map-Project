22425d074cef767a0f64122e036f7c05a87599b7
Sara Rykov <sara.rykov@gmail.com>
2018-04-27 10:04:41 -0400

    feature: Load Google Map

--- src/Map.js
+++ src/Map.js
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { Component } from 'react';
 
-function Map(props) {
+class Map extends Component {
 
-  return (
-    <div id="map"/>
+  componentDidMount() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: {lat: 40.7608, lng: -111.8910},
+      zoom: 5,
+      mapTypeControl: false
+    });
+  }
+
+  render() {
+    return (
+     <div id="map"/>
     )
+  }
 }
 
 export default Map
