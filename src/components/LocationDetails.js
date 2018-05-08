e5f9798e37e117a2d0c2bfdf61bf96e03f583485
Sara Rykov <sara.rykov@gmail.com>
2018-05-08 10:52:48 -0400

    feature: Display fouraquare data on Infowindow

--- src/components/LocationDetails.js
+++ src/components/LocationDetails.js
@@ -1,17 +1,49 @@
 import React, { Component } from 'react';
-//import * as RoutesAPI from '../util/RoutesAPI'
+import * as LocationAPI from '../util/LocationAPI';
 
 class LocationDetails extends Component {
 
+
+  constructor(props){
+    super(props);
+    this.state = {
+      item: {}
+    }
+  }
+
+  componentDidMount(){
+      LocationAPI.getLocationDetails(this.props.location.venue.id).then((location) => {
+      this.setState({
+        item:location
+      })
+    })
+  }
+
   render() {
+    let item = this.state.item
+    if(Object.keys(item).length === 0){
+     return(
+        <div className="info-window">Loading...</div>
+     )
+    }
+    else{
+     console.log(item)
+     const facebookUrl = `http://www.facebook.com/${item.contact.facebookUsername}`
+     const twitterUrl = `http://www.twitter.com/${item.contact.twitter}`
 
-   const location = this.props.location
+     return(
+        <div className="info-window">
+          <div className="location-info">
+            <div className="location-title">{item.name}</div>
+            <div className="location-detail">{item.categories[0].name} | {item.location.address}</div>
+            <div className="location-detail">{item.contact.formattedPhone != null && <span>{item.contact.formattedPhone} | </span>} <a href={item.url}>Website</a></div>
 
-   return(
-      <div className="info-window">
-        <span className="area-title">{location.venue.name}</span>
-      </div>
-   )
+            {item.contact.facebookUsername && <a href={facebookUrl}><i className="fab fa-facebook-square fa-2x"></i></a>}
+            {item.contact.twitter && <a href={twitterUrl}><i className="fab fa-twitter-square fa-2x"></i></a>}
+          </div>
+        </div>
+     )
+   }
  }
 
 }
--- src/util/LocationAPI.js
+++ src/util/LocationAPI.js
@@ -3,6 +3,7 @@ export const torontoLng = -79.383184
 
 const client_id='4HQKBFUPS0MOXK1EOLLV3CYVIY4XDGU2WI0WCQAFCYI2UAWG'
 const client_secret='P1LFYYR2HIBCLZDRFQA52HIHHM4SILROLR5WX2RC2BW0OLPH'
+
 const venuesEndpoint = 'https://api.foursquare.com/v2/venues/explore'
 const paramsForVenueRequest = {
 	ll: `${torontoLat},${torontoLng}`,
@@ -17,7 +18,7 @@ const paramsForVenueRequest = {
 
 
 export const getLocations = () => {
-	const venuesRequestUrl = `${venuesEndpoint}?${buildUrl(venuesEndpoint, paramsForVenueRequest)}`
+	const venuesRequestUrl = `${venuesEndpoint}?${buildQueryString(paramsForVenueRequest)}`
 
 	return fetch(venuesRequestUrl)
 	.then(response => response.json())
@@ -26,7 +27,29 @@ export const getLocations = () => {
 
 
 
-function buildUrl(url, parameters){
+//https://api.foursquare.com/v2/venues/VENUE_ID/photos
+//https://developer.foursquare.com/docs/api/venues/details
+
+
+
+const detailsEndpoint = 'https://api.foursquare.com/v2/venues/'
+const paramsForDetailsRequest = {
+	client_id: client_id,
+	client_secret: client_secret,
+	v:'20180507'
+}
+
+export const getLocationDetails = (venueId) => {
+	let detailsRequestUrl = `${detailsEndpoint}/${venueId}`
+	detailsRequestUrl = `${detailsRequestUrl}?${buildQueryString(paramsForDetailsRequest)}`
+
+	return fetch(detailsRequestUrl)
+	.then(response => response.json())
+    .then(data => data.response.venue)
+}
+
+
+function buildQueryString(parameters){
   let qs = "";
   for(let key in parameters) {
     let value = parameters[key];
